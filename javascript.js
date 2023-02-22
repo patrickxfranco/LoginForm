ee3be053cdfaa86fe31fc94c04d5980bf060ce8e
patrickxfranco <patrickxfranco@gmail.com>
2023-02-22 19:36:50 -0300

    Alteração no CSS e habilitando a mensagem JS

--- javascript.js
+++ javascript.js
@@ -1,14 +1,7 @@
-//const html = document.documentElement
-var a = document.getElementsByClassName("errorMessage")
-
-function mostrarNome() { 
-    alert(username.value)
-}
-
-if (username.value != "") {
-    alert("User: " + username.value);
-    alert("Password: " + password.value);
-} else { 
-    a.style.display =
-        'display: inline';
+function mostrarNome() {
+    //alert(username.value)
+    if (username.value != null) {
+        alert("User: " + username.value);
+        alert("Password: " + password.value);
+    }
 }
